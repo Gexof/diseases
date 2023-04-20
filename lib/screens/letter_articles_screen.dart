3a7ae7aff3f78271f2e102226ae87227e9cecbd3
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-04-20 03:06:29 +0200

    Category Letter Search Done

--- lib/screens/letter_articles_screen.dart
+++ lib/screens/letter_articles_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:medicare_health_app/controllers/articles_controller.dart';
 
 import 'package:medicare_health_app/widgets/custom_app_bar.dart';
+import 'package:provider/provider.dart';
 
 import '../constants/strings.dart';
+import '../models/article_model.dart';
 import '../widgets/grid_builder.dart';
 import '../widgets/square_card.dart';
 import 'about_disease_screen.dart';
 
-class LetterArticlesScreen extends StatelessWidget {
+class LetterArticlesScreen extends StatefulWidget {
   const LetterArticlesScreen({
     required this.letter,
     super.key,
@@ -16,9 +19,23 @@ class LetterArticlesScreen extends StatelessWidget {
   final String letter;
 
   @override
+  State<LetterArticlesScreen> createState() => _LetterArticlesScreenState();
+}
+
+class _LetterArticlesScreenState extends State<LetterArticlesScreen> {
+  List<Article> filteredArticles = [];
+
+  @override
+  void initState() {
+    filteredArticles = Provider.of<ArticlesController>(context, listen: false)
+        .getAlphabeticCategory(letter: widget.letter);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: '$letter-Diseases'),
+      appBar: CustomAppBar(title: '${widget.letter}-Diseases'),
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         child: Column(
@@ -26,7 +43,7 @@ class LetterArticlesScreen extends StatelessWidget {
           children: [
             GridBuilder(
               gridMap: imgs,
-              itemCount: imgs.length,
+              itemCount: filteredArticles.length,
               itemBuilder: (context, index) {
                 return GestureDetector(
                   onTap: () {
@@ -37,7 +54,7 @@ class LetterArticlesScreen extends StatelessWidget {
                   },
                   child: SquareCard(
                     img: "${imgs[index]['img']}",
-                    title: "${imgs[index]['title']}",
+                    title: filteredArticles[index].title,
                     height: 124,
                     width: 167,
                   ),
